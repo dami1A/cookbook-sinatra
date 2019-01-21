bfc7c854bc22e2be46dc144f79a82fd770b47569
damien arnaud <darnaud@mail.com>
2019-01-21 16:13:04 +0100

    Boilerplate sinatra app

---
+++ app.rb
@@ -0,0 +1,12 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "pry-byebug"
+require "better_errors"
+configure :development do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = File.expand_path('..', __FILE__)
+end
+
+get '/' do
+  "That is my first web server and my second HTTP request to it*!\n*after 'Hello World!'"
+end
